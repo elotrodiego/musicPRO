d5295f09537c83d64d59bcd878bc47458adae05c
elotrodiego <elotrodiegoluna@gmail.com>
2021-07-08 10:36:13 -0400

    cambiado el name del login de api

--- api_juegos/urls.py
+++ api_juegos/urls.py
@@ -3,7 +3,7 @@ from .views import apiJuegos, juego
 from .loginview import login
 
 urlpatterns = [
-    path('juego/', apiJuegos, name="juegos"),
-    path('juego/<pk>', juego, name="juego"),
-    path('login/', login, name='login'),
+    path('juego/', apiJuegos, name="apijuegos"),
+    path('juego/<pk>', juego, name="apijuego"),
+    path('login/', login, name='apilogin'),
 ]
