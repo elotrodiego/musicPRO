f8be32623a78c2f38908d885dc9d6ad7d6a90076
elotrodiego <elotrodiegoluna@gmail.com>
2022-04-03 18:34:24 -0400

    1

--- core/admin.py
+++ core/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Genero, Juego, Desarrollador, Editor
+from .models import Genero, Juego, Desarrollador, Editor, Producto
 
 # Register your models here.
 admin.site.register(Genero)
 admin.site.register(Juego)
 admin.site.register(Desarrollador)
 admin.site.register(Editor)
+admin.site.register(Producto)
